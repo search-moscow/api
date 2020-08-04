f4bf6f0469f84940a1dda3977520c59fb97bd6dc
Dmitry <d.girsky@gmail.com>
2020-08-04 17:29:14 +0300

    integrate websocket server

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 const MongoClient = require('mongodb');
-const WebSocket = require('ws');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -66,33 +65,6 @@ app.use('/api/events', eventsRouter);
 app.use('/api/districts', districtsRouter);
 app.use('/api/widget', widgetRouter);
 
-// Simple server
-const wss = new WebSocket.Server({ port: 5050 });
 
-wss.on('connection', function connection(ws) {
-  console.log('user connect')
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  
-  ws.send(formatAMPM(new Date));
-
-  setInterval(function timeout() {
-    ws.send(formatAMPM(new Date));
-  }, 1000);
-
-});
-
-
-function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-  }
 
 module.exports = app;
