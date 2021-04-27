3bb9687bde852d23a6991ab741e15672d4f46d61
Dmitrii Girskii <d.girsky@gmail.com>
2021-04-27 21:05:32 +0300

    search products

--- dao/search.dao.js
+++ dao/search.dao.js
@@ -5,6 +5,7 @@ let realestates
 let events
 let shops
 let services
+let products
 let moscow
 
 class RestaurantDAO {
@@ -13,11 +14,12 @@ class RestaurantDAO {
         try {
             // process.env.MFLIX_NS
             moscow = await conn.db('moscow')
-            restaurants = await moscow.collection("restaurants")
-            realestates = await moscow.collection("realestates")
-            events = await moscow.collection("events")
-            shops = await moscow.collection("shops")
-            services = await moscow.collection("services")
+            // restaurants = await moscow.collection("restaurants")
+            // realestates = await moscow.collection("realestates")
+            // events = await moscow.collection("events")
+            // shops = await moscow.collection("shops")
+            // services = await moscow.collection("services")
+            products = await moscow.collection("products")
         //   this.movies = movies // this is only for testing
         } catch (e) {
           console.error(
@@ -27,42 +29,49 @@ class RestaurantDAO {
     }
 
     static async search(text) {
-        const resultsRestaurants = await restaurants
-            .aggregate([
-                { $match: { $text: { $search: text } } },
-                { $addFields: { "dao":"restaurants" }},
-                { $sort: { score: { $meta: "textScore" } } },
-            ]).toArray();
+        // const resultsRestaurants = await restaurants
+        //     .aggregate([
+        //         { $match: { $text: { $search: text } } },
+        //         { $addFields: { "dao":"restaurants" }},
+        //         { $sort: { score: { $meta: "textScore" } } },
+        //     ]).toArray();
 
-        const resultsRealestates = await realestates
-            .aggregate([
-                { $match: { $text: { $search: text } } },
-                { $addFields: { "dao":"realestates" }},
-                { $sort: { score: { $meta: "textScore" } } },
-            ]).toArray();
+        // const resultsRealestates = await realestates
+        //     .aggregate([
+        //         { $match: { $text: { $search: text } } },
+        //         { $addFields: { "dao":"realestates" }},
+        //         { $sort: { score: { $meta: "textScore" } } },
+        //     ]).toArray();
               
-        const resultsEvents = await events
-            .aggregate([
-                { $match: { $text: { $search: text } } },
-                { $addFields: { "dao":"events" }},
-                { $sort: { score: { $meta: "textScore" } } },
-            ]).toArray();
+        // const resultsEvents = await events
+        //     .aggregate([
+        //         { $match: { $text: { $search: text } } },
+        //         { $addFields: { "dao":"events" }},
+        //         { $sort: { score: { $meta: "textScore" } } },
+        //     ]).toArray();
 
-        const resultsShops = await shops
-            .aggregate([
-                { $match: { $text: { $search: text } } },
-                { $addFields: { "dao":"shops" }},
-                { $sort: { score: { $meta: "textScore" } } },
-            ]).toArray();
+        // const resultsShops = await shops
+        //     .aggregate([
+        //         { $match: { $text: { $search: text } } },
+        //         { $addFields: { "dao":"shops" }},
+        //         { $sort: { score: { $meta: "textScore" } } },
+        //     ]).toArray();
 
-        const resultsServices = await services
+        // const resultsServices = await services
+        //     .aggregate([
+        //         { $match: { $text: { $search: text } } },
+        //         { $addFields: { "dao":"services" }},
+        //         { $sort: { score: { $meta: "textScore" } } },
+        //     ]).toArray();
+
+        const resultsProducts = await products
             .aggregate([
                 { $match: { $text: { $search: text } } },
-                { $addFields: { "dao":"services" }},
+                { $addFields: { "dao":"products" }},
                 { $sort: { score: { $meta: "textScore" } } },
             ]).toArray();
             
-        const results = [].concat(resultsRestaurants, resultsRealestates, resultsEvents, resultsShops, resultsServices)
+        const results = [].concat(resultsProducts)
 
         if (results) {
             console.log(`Found a listing in the collection:'`);
